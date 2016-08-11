49c205fabf7fb3ee884353830504e30be9123509
tobros91 <tobias@bechwebb.se>
2016-08-11 18:23:58 +0200

    Filtered output from Lang:getLoader if empty file and create file if it's not there

--- src/UFirst/LangImportExport/LangListService.php
+++ src/UFirst/LangImportExport/LangListService.php
@@ -13,13 +13,40 @@ class LangListService {
 	}
 
 	public function writeLangList($locale, $group, $new_translations) {
+
+
+
 		$translations = Lang::getLoader()->load($locale, $group);
+
+
+		// If lang file exists but is empty we reset $translations cuz somehow it get's returned as int 1
+		if(!is_array($translations)) {
+			$translations = array();
+		}
+
+
 		foreach($new_translations as $key => $value) {
 			array_set($translations, $key, $value);
 		}
-		$header = "<?php\n\nreturn ";
+
+
+
+		
 
 		$language_file = base_path("resources/lang/{$locale}/{$group}.php");
+
+		// If file does not exist create an empty so is_writable() function does not fail
+
+		if(!file_exists($language_file)) {
+
+			$fp = fopen($language_file, 'w');
+			fclose($fp);
+			
+		}
+
+		$header = "<?php\n\nreturn ";
+	
+
 		if (is_writable($language_file) && ($fp = fopen($language_file, 'w')) !== FALSE) {
 
 			fputs($fp, $header.var_export($translations[$group], TRUE).";\n");
